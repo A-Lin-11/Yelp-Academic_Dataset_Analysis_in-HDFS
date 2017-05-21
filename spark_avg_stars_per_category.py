b192e4815495e39096b6d4141611b541aac4fb4e
Andrew Lin <andrewlinster@gmail.com>
2017-05-21 17:51:35 -0400

    Add files via upload

---
+++ spark_avg_stars_per_category.py
@@ -0,0 +1,34 @@
+# Calculate the average stars for each business category
+# Written by Dr. Yuhang Wang and Josh Gardner
+'''
+To run on Fladoop cluster:
+spark-submit --master yarn-client --queue si618w17 --num-executors 16 --executor-memory 1g --executor-cores 2 spark_avg_stars_per_category.py
+
+To get results:
+hadoop fs -getmerge avg_stars_per_category_output avg_stars_per_category_output.txt
+'''
+import json
+from pyspark import SparkContext
+sc = SparkContext(appName="PySparksi618w17avg_stars_per_category")
+
+input_file = sc.textFile("hdfs:///var/si618w17/yelp_academic_dataset_business_updated.json")
+
+def cat_star(data):
+  cat_star_list = []
+  stars = data.get('stars', None)
+  categories = data.get('categories', None)
+  if categories:
+    for c in categories:
+      if stars != None:
+        cat_star_list.append((c, stars))
+  return cat_star_list
+
+
+cat_stars = input_file.map(lambda line: json.loads(line)) \
+                      .flatMap(cat_star) \
+                      .mapValues(lambda x: (x, 1)) \
+                      .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])) \
+                      .map(lambda x: (x[0], x[1][0]/x[1][1]))
+
+cat_stars.collect()
+cat_stars.saveAsTextFile("avg_stars_per_category_output")
